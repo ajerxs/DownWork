596749c86eaa0082066b23c69d12c1d645780092
Andrew Eames <ajerxs@gmail.com>
2021-10-21 14:40:30 -0400

    Workers controller and views, building client partial

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,3 @@
 class Client < User
     has_many :jobs
-
-    def full_name
-        "#{self.first_name} " + "#{self.last_name}"
-    end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :jobs
     has_secure_password
+
+    def full_name
+        "#{self.first_name} " + "#{self.last_name}"
+    end
+
 end
