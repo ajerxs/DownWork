31a6b9d29e1042a9da8c477926f2e99222f0fd67
Andrew Eames <ajerxs@gmail.com>
2021-10-26 18:08:47 -0400

    Home nav button added to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,5 @@ class ApplicationController < ActionController::Base
 
     def home
     end
-
-    # helpers
-    
-    # def user_type
-    #     user = User.find_by(id: session[:user_id])
-    #     user.type
-    # end
     
 end
